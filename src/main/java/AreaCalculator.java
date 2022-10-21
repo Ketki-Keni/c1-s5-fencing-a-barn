91f8de163721e2f836a4eb8db148878aed8ec0ba
Ketki Keni <sketki220@gmail.com>
2022-10-21 21:46:13 +0530

    called methods in main method

--- src/main/java/AreaCalculator.java
+++ src/main/java/AreaCalculator.java
@@ -2,7 +2,18 @@ import java.util.Scanner;
 
 public class AreaCalculator {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the side of the Square : ");
+        double side = scanner.nextDouble();
+        System.out.println("Square Area : " + calculateAreaOfSquare(side));
+        System.out.print("Enter the radius of the Circle : ");
+        double radius = scanner.nextDouble();
+        System.out.println("Circle Area : " + calculateAreaOfCircle(radius));
+        System.out.print("Enter the length of the Rectangle : ");
+        double length = scanner.nextDouble();
+        System.out.print("Enter the breadth of the Rectangle : ");
+        double breadth = scanner.nextDouble();
+        System.out.println("Rectangle Area : " + calculateAreaOfRectangle(length, breadth));
     }
     public static double calculateAreaOfSquare(double side){
         return side * side;
