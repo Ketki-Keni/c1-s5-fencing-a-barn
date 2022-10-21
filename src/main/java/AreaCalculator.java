9f996ed91287b521747bd003bfd58fbb20c19eb6
Ketki Keni <sketki220@gmail.com>
2022-10-21 21:45:15 +0530

    added method calculateAreaOfSquare, calculateAreaOfCircle and calculateAreaOfRectangle

--- src/main/java/AreaCalculator.java
+++ src/main/java/AreaCalculator.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class AreaCalculator {
+    public static void main(String[] args) {
+
+    }
+    public static double calculateAreaOfSquare(double side){
+        return side * side;
+    }
+    public static double calculateAreaOfCircle(double radius){
+        return 3.14 * radius * radius;
+    }
+    public static double calculateAreaOfRectangle(double length, double breadth){
+        return length * breadth;
+    }
 }
